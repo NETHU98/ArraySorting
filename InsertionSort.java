a9a100e862118c128d894365740927b86e89bf4d
DudumU <62570762+NETHU98@users.noreply.github.com>
2023-03-10 22:30:49 -0500

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,14 +1,5 @@
 import java.util.Scanner;
 
-/**
- * @author nethmeeperera
- * @version 02/02/2023
- * CSC2720 Lab #21093
- * lab time : Wednesday 16:00-16:50
- * due on 02/05/2023
- */
-import java.util.Scanner;
-
 public class InsertionSort {
     public static void sort(int arr[]) {
         int n = arr.length;
